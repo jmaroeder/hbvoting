beb60a5a2fe4bd8e7c1638b566796e2d41a46755
James M. Allen <james.m.allen@gmail.com>
2012-10-23 10:28:11 -0400

    init db code

--- hbvoting.py
+++ hbvoting.py
@@ -1,6 +1,7 @@
 from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
 		render_template, flash, _app_ctx_stack
+from contextlib import closing
 
 
 app = Flask(__name__)
@@ -9,8 +10,15 @@ app.config.from_pyfile('settings.py')
 
 
 def connect_db():
-	return sqlite3.connect(app.config['DATABASE'])
+	conn = sqlite3.connect(app.config['DATABASE'])
+	conn.execute('pragma foreign_keys = on')
+	return conn
 
+def init_db():
+	with closing(connect_db()) as db:
+		with app.open_resource('schema.sql') as f:
+			db.cursor().executeScript(f.read())
+		db.commit()
 
 
 
